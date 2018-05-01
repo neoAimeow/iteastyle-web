60cb76588e7b18bc52f7b625f8daddf3f301e525
cross <a278886971@gmail.com>
2018-05-01 16:50:04 +0800

    shell frame

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,48 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="home-container">
+      <div class="home-top">
+        <div class="home-top-left"></div>
+        <div class="home-top-middle">
+          <menus v-for="(section,key) in menus" :key="key" :label="key">
+              <menu-item v-for="(menu,index) in section" :key="index" :icon="menu.icon" :is-active="true" :to="{ path: menu.path, query: {}}">
+                <span>{{menu.title}}</span>
+              </menu-item>
+            </menus>
+        </div>
+        <div class="home-top-right"></div>
+      </div>
+      <div class="home-center">
+        <router-vie>
+        <div class="home-center-top"></div>
+        <div class="home-center-middle"></div>
+        <div class="home-center-foot"></div>
+        </router-vie>
+      </div>
+      <div class="home-foot">
+        <div class="home-foot-top">
+          <div class="home-foot-top-left"></div>
+          <div class="home-foot-top-right"></div>
+        </div>
+        <div class="home-foot-bottom">
+          <div class="home-foot-bottom-left"></div>
+          <div class="home-foot-bottom-right"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.home-top{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import home from '@/components/Home'
+
+import SiteIndex from '@components/site-manager/index'
+import CompanyStory from '@components/site-manager/CompanyStory'
+import ProductDisplay from '@components/site-manager/ProductDisplay'
+import ServiceContent from '@components/site-manager/ServiceContent'
+import ClassicCase from '@components/site-manager/ClassicCase'
+import TeaStatus from '@components/site-manager/TeaStatus'
+import ContactUs from '@components/site-manager/ContactUs'
 
 Vue.use(Router)
 
@@ -8,8 +16,43 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: Home
+    },
+    {
+      path: 'site-manager/site-index',
+      name: 'site-index',
+      component: SiteIndex
+    },
+    {
+      path: '/site-manager/company-story',
+      name: 'company-story',
+      component: CompanyStory
+    },
+    {
+      path: '/site-manager/product-display',
+      name: 'product-display',
+      component: ProductDisplay
+    },
+    {
+      path: '/site-manager/service-content',
+      name: 'service-content',
+      component: ServiceContent
+    },
+    {
+      path: '/site-manager/classic-case',
+      name: 'classic-case',
+      component: ClassicCase
+    },
+    {
+      path: '/site-manager/tea-status',
+      name: 'tea-status',
+      component: TeaStatus
+    },
+    {
+      path: '/site-manager/contact-us',
+      name: 'contact-us',
+      component: ContactUs
     }
   ]
 })
