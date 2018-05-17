c1703466a676fb0677a1dcb01e2fc37952b99451
cross <a278886971@gmail.com>
2018-05-18 00:29:58 +0800

    update this code

--- src/components/homepage/index.vue
+++ src/components/homepage/index.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <el-carousel height="1000px">
+  <el-carousel>
     <el-carousel-item v-for="item in items" :key="item">
       <progressive-img :src="item" alt="" style="width:100%;" />
     </el-carousel-item>
@@ -21,13 +21,15 @@
 </div>
 </template>
 <script>
+import af1 from '../../assets/AfternoonTea.jpeg';
+import af2 from '../../assets/AfternoonTea.jpeg';
+import af3 from '../../assets/AfternoonTea.jpeg';
+
 export default {
   data () {
     return {
       items : [
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1525889999049&di=e15c3b502ad339dc0577cb1451a86b2f&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01ff555938ac18a8012193a373ba4c.png%402o.png',
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1525889999046&di=d3de9534ee651859623d517cd594e56b&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F018a73555c72670000009c50a19736.jpg%40900w_1l_2o_100sh.jpg',
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1525889999039&di=d7a0e0dc52562b87d183f5da36858e15&imgtype=0&src=http%3A%2F%2Fpic.35pic.com%2Fnormal%2F09%2F33%2F13%2F8498245_112506028322_2.jpg'
+        af1,af2,af3
       ],
       desserts : [
         'https://gss3.bdstatic.com/7Po3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike150%2C5%2C5%2C150%2C50/sign=bff7b854c3fdfc03f175ebeab556ecf1/83025aafa40f4bfb8ce6d0fe034f78f0f7361861.jpg',
