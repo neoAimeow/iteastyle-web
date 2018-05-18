29e51287fbc98cc4bb7bf4c4dcf2a846dd0799f0
sharon <403431641@qq.com>
2018-05-19 02:14:04 +0800

    update

--- src/components/homepage/ClassicCase.vue
+++ src/components/homepage/ClassicCase.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="cc-container">
+
     <router-link to="/homepage/case-insidepage1">
+    <div style="width:200px;height:450px;">
+
+      <div style="width:200px;height:300px;background-img:url(../../assets/tea-case-images.png);">
+    
+        <img src="../../assets/teacase-light-logo.png" style="width:40%;z-index:1;">
+      </div>
+      
+      <div calss="case-foot" style="width:200px;height:150px;display:flex;align-items:center;justify-content:center;background-color:yellow;">
+        <img src="../../assets/case-tea-dessert.png" style="width:50%">
+      </div>
+
+    </div>      
+    </router-link>
+    
+    
+  </div>
+
+
+
+
+
+
+    <!--<router-link to="/homepage/case-insidepage1">
     <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1526403898400&di=dadaeaf28bc0df9132c5953a4a8c70c3&imgtype=0&src=http%3A%2F%2Fgb.cri.cn%2Fmmsource%2Fimages%2F2007%2F04%2F11%2Fwc070411032.jpg" alt="">
     </router-link>
     <router-link to="/homepage/case-insidepage1">
@@ -14,8 +38,8 @@
     </router-link>
     <router-link to="/homepage/case-insidepage1">
     <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1526403898400&di=dadaeaf28bc0df9132c5953a4a8c70c3&imgtype=0&src=http%3A%2F%2Fgb.cri.cn%2Fmmsource%2Fimages%2F2007%2F04%2F11%2Fwc070411032.jpg" alt="">
-    </router-link>
-    </div>
+    </router-link>-->
+  
 </template>
 
 <style lang="scss">
@@ -27,8 +51,11 @@
   margin-top: 30px;
   margin: 0 auto;
 }
-.cc-container img{
-  width: 200px;
-  height: 400px;
+//.cc-container img{
+  //width: 200px;
+  //height: 400px;
+//}
+.case-foot{
+
 }
 </style>
