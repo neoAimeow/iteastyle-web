b3b615e4a6e2d46602ca22c429938439dd529e32
sharon <403431641@qq.com>
2018-05-18 20:01:21 +0800

    update

--- src/components/homepage/TeaState.vue
+++ src/components/homepage/TeaState.vue
@@ -1,34 +1,46 @@
 <template>
   <div class="state-container">
   <div style="margin-top:100px;wdith:1000px;height:1000px">
-    <router-link to="state-insidepage" class="state">
-      <img src="https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=67fb47b01e178a82da3177f2976a18e8/902397dda144ad34bc5f3afbd0a20cf430ad85ee.jpg" alt="" style="min-height:80px;min-width:80px;height:80px;width:80px;border-radius:50px;border:solid 5px rgba(155,194,72,0.5);">
-      <div class="state-intro">
-        <p style="color:#9bc348">这份小确幸，您想要吗？</p>
-        <p style="color:white;font-size:12px">转眼夏至 这一年已过去一半 正值夏天 闷热、阵雨、烈日 似乎是个不太好的季节 却也有人喜欢夏天的激情 阳光、海滩、冷饮 激情的夏日 一切活动仿佛充满了干劲...</p>
+    <div class="tea-state">     
+      <img class="images" src="../../assets/state-image.png" alt="">
+      <div class="state">
+        <div style="display:flex;flex-direction:column;align-content:center;justify-content:center;width:700px;height:120px;margin-left:90px">
+        <span style="color:#9bc348;font-size:16px;width:600px">这份小确幸，您想要吗？</span>
+        <span style="color:white;font-size:13px;margin-top:10px;width:600px">转眼夏至 这一年已过去一半 正值夏天 闷热、阵雨、烈日 似乎是个不太好的季节 却也有人喜欢夏天的激情 阳光、海滩、冷饮 激情的夏日 一切活动仿佛充满了干劲...</span>
+        </div>
+        <router-link to="state-insidepage"><img src="../../assets/+.png" style="margin-top:97.5px;"></router-link>
       </div>
-    </router-link>
-    <router-link to="state-insidepage" class="state">
-      <img src="https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=67fb47b01e178a82da3177f2976a18e8/902397dda144ad34bc5f3afbd0a20cf430ad85ee.jpg" alt="" style="height:80px;width:80px;border-radius:50px;border:solid 5px rgba(155,194,72,0.5); ">
-      <div class="state-intro">
-        <p style="color:#9bc348"> 这份小确幸，您想要吗？</p>
-        <p style="color:white">从文献上看来，点心与茶食两者原有区别，性质也就不同，但是后来早已混同了。</p>
+    </div> 
+    <div class="tea-state">     
+      <img class="images" src="../../assets/state-image.png" alt="">
+      <div class="state">
+        <div style="display:flex;flex-direction:column;align-content:center;justify-content:center;width:700px;height:120px;margin-left:90px">
+        <span style="color:#9bc348;font-size:16px;width:600px">这份小确幸，您想要吗？</span>
+        <span style="color:white;font-size:13px;margin-top:10px;width:600px">转眼夏至 这一年已过去一半 正值夏天 闷热、阵雨、烈日 似乎是个不太好的季节 却也有人喜欢夏天的激情 阳光、海滩、冷饮 激情的夏日 一切活动仿佛充满了干劲...</span>
+        </div>
+        <router-link to="state-insidepage"><img src="../../assets/+.png" style="margin-top:97.5px;"></router-link>
       </div>
-    </router-link>
-    <router-link to="state-insidepage" class="state">
-      <img src="https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=67fb47b01e178a82da3177f2976a18e8/902397dda144ad34bc5f3afbd0a20cf430ad85ee.jpg" alt="" style="height:80px;width:80px;border-radius:50px;border:solid 5px rgba(155,194,72,0.5);">
-      <div class="state-intro">
-        <p style="color:#9bc348">这份小确幸，您想要吗？</p>
-        <p style="color:white">从文献上看来，点心与茶食两者原有区别，性质也就不同，但是后来早已混同了。</p>
+    </div>
+    <div class="tea-state">     
+      <img class="images" src="../../assets/state-image.png" alt="">
+      <div class="state">
+        <div style="display:flex;flex-direction:column;align-content:center;justify-content:center;width:700px;height:120px;margin-left:90px">
+        <span style="color:#9bc348;font-size:16px;width:600px">这份小确幸，您想要吗？</span>
+        <span style="color:white;font-size:13px;margin-top:10px;width:600px">转眼夏至 这一年已过去一半 正值夏天 闷热、阵雨、烈日 似乎是个不太好的季节 却也有人喜欢夏天的激情 阳光、海滩、冷饮 激情的夏日 一切活动仿佛充满了干劲...</span>
+        </div>
+        <router-link to="state-insidepage"><img src="../../assets/+.png" style="margin-top:97.5px;"></router-link>
       </div>
-    </router-link>
-    <router-link to="state-insidepage" class="state">
-      <img src="https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=67fb47b01e178a82da3177f2976a18e8/902397dda144ad34bc5f3afbd0a20cf430ad85ee.jpg" alt="" style="height:80px;width:80px;border-radius:50px;border:solid 5px rgba(155,194,72,0.5);">
-      <div class="state-intro">
-        <p style="color:#9bc348">这份小确幸，您想要吗？</p>
-        <p style="color:white">从文献上看来，点心与茶食两者原有区别，性质也就不同，但是后来早已混同了。</p>
+    </div>
+    <div class="tea-state">     
+      <img class="images" src="../../assets/state-image.png" alt="">
+      <div class="state">
+        <div style="display:flex;flex-direction:column;align-content:center;justify-content:center;width:700px;height:120px;margin-left:90px">
+        <span style="color:#9bc348;font-size:16px;width:600px">这份小确幸，您想要吗？</span>
+        <span style="color:white;font-size:13px;margin-top:10px;width:600px">转眼夏至 这一年已过去一半 正值夏天 闷热、阵雨、烈日 似乎是个不太好的季节 却也有人喜欢夏天的激情 阳光、海滩、冷饮 激情的夏日 一切活动仿佛充满了干劲...</span>
+        </div>
+        <router-link to="state-insidepage"><img src="../../assets/+.png" style="margin-top:97.5px;"></router-link>
       </div>
-    </router-link>
+    </div>
   </div>
   </div>
 </template>
@@ -40,22 +52,37 @@
   background-position:center;
   background-color: #23201f;
   width: 100%;
-  height: 800px;
+  height: 900px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  //background-repeat: no-repeat;
+  background-repeat: no-repeat;
 }
 .state{
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  height: 90px;
-  margin: 0 auto;
-  margin-top: 30px;
+  height: 120px;
   background-color: rgba(17,15,30,0.5);
-  width: 700px;
+  width: 750px;
   //border-radius: 40px;
+
+}
+.tea-state{
+height:120px;
+width: 810px;
+//background-color:red;
+display:flex;
+flex-direction: row;
+margin-top:30px;
+}
+.images{
+height:100px;
+width:100px;
+border-radius:50%;
+border:solid 10px rgba(155,194,72,0.6);
+margin-right:-60px;
+z-index:1;
 }
 </style>
