0f3f00c706d310184814a32777fb955a4a54c7c6
cross <a278886971@gmail.com>
2018-06-14 01:04:52 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <div class="home-container">
-    <div class="home-head">
-      <el-row type="flex" justify="center">
-        <el-col :span="12"><img src="*" alt="公司logo"></el-col>
-        <el-col :span="12">
-          <el-menu active-text-color="#81b316" default-active="首页" class="el-menu-demo" :router="true" mode="horizontal">
-            <el-menu-item :index="data.title" v-for="(data,key) in menus" :key="key" :label="key" :route="data.path">
-              {{data.title}}
-            </el-menu-item>
-          </el-menu>
-        </el-col>
-      </el-row>
-    </div>
-    <div>
-      <router-view></router-view>
-    </div>
-    <div class="home-btm">
-      <div class="btm-top">
-        <ul class="btm-top-left" v-for="(data,key) in btmmenus" :key="key" :label="key">
-          <li v-for="(menu,index) in data" :key="index">
-            <router-link :to="menu.path" :style="menu.style">{{menu.title}}</router-link>
-          </li>
-        </ul>
-        <div class="btm-top-right">
-          <p style="font-size: 12px;">全国订购电话</p>
-          <p style="font-size: 27px; margin-top: 10px;">400 688 6888</p>
+  <div id="app">
+    <div class="home-container">
+      <div class="home-head">
+        <div class="home-head-left"><img src="*" alt="公司logo"></div>
+        <div class="home-head-right">
+          <router-link class="home-head-right-list" :index="data.title" v-for="(data,key) in menus" :key="key" :label="key" :to="data.path">
+            {{data.title}}
+          </router-link>
+        </div>
+      </div>
+      <div><router-view></router-view></div>
+      <div class="home-bottom">
+        <div class="btm-top">
+          <ul class="btm-top-left" v-for="(data,key) in btmmenus" :key="key" :aria-label="key">
+            <li class="btm-top-left-list" v-for="(menu,index) in data" :key="index">
+              <router-link :to="menu.path" :style="menu.style">{{menu.title}}</router-link>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -35,12 +27,12 @@
 import menus from './common/menus'
 import btmmenus from './common/btmmenus'
 export default {
-  name: 'APP',
+  name: 'App',
   data(){
     return{
-      menus:[],
-      btmmenus:[]
-    }
+    menus: [],
+    btmmenus: []
+  }
   },
   created(){
     this.menus = menus
@@ -50,17 +42,36 @@ export default {
 </script>
 
 <style lang="scss">
-html,
-body,
-.home-container{
-  margin: 0;
-  min-width: 950px;
-  font-family: "Lantinghei SC";
-  color: #626262;
+a {
+  text-decoration: none;
+}
+.router-link-active {
+  text-decoration: none;
+  color: #81b316;
 }
 ul,li{
   list-style: none;
-  padding: 0;
   margin: 0;
+  padding: 0;
+}
+html,
+body,
+#app{
+  margin:0;
+}
+.home-head{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+.home-head-left{
+  background-color: red;
+}
+.home-head-right{
+  display: flex;
+  flex-direction: row;
+}
+.home-head-right-list{
+  margin-left: 20px;
 }
 </style>
--- src/components/homepage/index.vue
+++ src/components/homepage/index.vue
@@ -1,3 +1,3 @@
 <template>
-    <h1>111111</h1>
+    <h1>首页</h1>
 </template>
