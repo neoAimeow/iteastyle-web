6db04d6624ef2d6c3f690e88aebcec7f81bb16e0
Cross <cross@CrossdeMacBook-Pro.local>
2018-06-14 15:42:36 +0800

    update

--- src/components/homepage/index.vue
+++ src/components/homepage/index.vue
@@ -1,13 +1,35 @@
 <template>
-    <el-container>
-        <el-header>
-            <el-carousel>
-                <el-carousel-item v-for="item in 4" :key="item">
-                    {{img}}
-                </el-carousel-item>
-            </el-carousel>
-        </el-header>
-        <el-main></el-main>
-        <el-footer></el-footer>
-    </el-container>
+    <div class="index-container">
+        <div class="index-head">
+            <img src="http://pa74otoy6.bkt.clouddn.com/%E6%8A%B9%E8%8C%B6%E8%B4%B9%E5%8D%97%E9%9B%AA%E4%BA%A7%E5%93%81%E5%86%85%E9%A1%B5%E7%BD%91%E7%BB%9C%E5%9B%BE%E7%89%87.jpg" alt="" />
+        </div>
+        <div class="index-main">
+            <img src="http://pa74otoy6.bkt.clouddn.com/%E6%8A%B9%E8%8C%B6%E8%B4%B9%E5%8D%97%E9%9B%AA%E4%BA%A7%E5%93%81%E5%86%85%E9%A1%B5%E7%BD%91%E7%BB%9C%E5%9B%BE%E7%89%87.jpg" />
+            <img src="http://pa74otoy6.bkt.clouddn.com/%E6%8A%B9%E8%8C%B6%E8%B4%B9%E5%8D%97%E9%9B%AA%E4%BA%A7%E5%93%81%E5%86%85%E9%A1%B5%E7%BD%91%E7%BB%9C%E5%9B%BE%E7%89%87.jpg" />
+            <img src="http://pa74otoy6.bkt.clouddn.com/%E6%8A%B9%E8%8C%B6%E8%B4%B9%E5%8D%97%E9%9B%AA%E4%BA%A7%E5%93%81%E5%86%85%E9%A1%B5%E7%BD%91%E7%BB%9C%E5%9B%BE%E7%89%87.jpg" />
+        </div>
+        <div class="index-foot">
+            
+        </div>
+    </div>
 </template>
+
+
+<style lang="scss">
+.index-head{
+    margin-top: 40px;
+    text-align: center;
+}
+.index-main{
+    margin-top: 40px;
+}
+.index-head img{
+    width: auto;
+    height: auto;
+}
+.index-main img{
+    height: 300px;
+    width: auto;
+    margin-left: 20px;
+}
+</style>
