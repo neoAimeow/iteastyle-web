54d36ef00d8245e08032a787fe7de0d9240fcc2a
Cross <cross@CrossdeMacBook-Pro.local>
2018-06-14 10:56:07 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,38 @@
       </div>
       <div><router-view></router-view></div>
       <div class="home-bottom">
-        <div class="btm-top">
-          <ul class="btm-top-left" v-for="(data,key) in btmmenus" :key="key" :aria-label="key">
-            <li class="btm-top-left-list" v-for="(menu,index) in data" :key="index">
+        <div class="home-btm-top">
+          <ul class="home-btm-top-left" v-for="(data,key) in btmmenus" :key="key" :aria-label="key">
+            <li class="home-btm-top-left-list" v-for="(menu,index) in data" :key="index">
               <router-link :to="menu.path" :style="menu.style">{{menu.title}}</router-link>
             </li>
+          </ul>
+          <div class="home-btm-top-right">
+            <p>全国订购电话</p>
+            <p>400 688 6888</p>
+          </div>
+        </div>
+        <hr/>
+        <div class="home-btm-foot">
+          <ul class="home-btm-foot-left">
+            <li>联系我们</li>
+            <li>|</li>
+            <li>使用条款</li>
+            <li>|</li>
+            <li>法律声明</li>
+          </ul>
+          <ul class="home-btm-foot-right">
+            <li>Company</li>
+            <li>|</li>
+            <li>Product</li>
+            <li>|</li>
+            <li>Service</li>
+            <li>|</li>
+            <li>Classic case</li>
+            <li>|</li>
+            <li>News</li>
+            <li>|</li>
+            <li>Contact</li>
           </ul>
         </div>
       </div>
@@ -47,7 +74,6 @@ a {
 }
 .router-link-active {
   text-decoration: none;
-  color: #81b316;
 }
 ul,li{
   list-style: none;
@@ -63,6 +89,7 @@ body,
   display: flex;
   flex-direction: row;
   justify-content: space-around;
+  min-width: 1000px;
 }
 .home-head-left{
   background-color: red;
@@ -73,5 +100,30 @@ body,
 }
 .home-head-right-list{
   margin-left: 20px;
+}
+.home-btm-top{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-right: 20px;
+}
+.home-btm-top-left{
+  margin-right: 40px;
+}
+.home-btm-top-right{
+  margin-left: 100px;
+}
+.home-btm-foot{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+.home-btm-foot-left{
+  display: flex;
+  justify-content: center;
+}
+.home-btm-foot-right{
+  display: flex;
+  justify-content: center;
 }
 </style>
--- src/components/homepage/index.vue
+++ src/components/homepage/index.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>首页</h1>
+    <el-container>
+        <el-header>
+            <el-carousel>
+                <el-carousel-item v-for="item in 4" :key="item">
+                    {{img}}
+                </el-carousel-item>
+            </el-carousel>
+        </el-header>
+        <el-main></el-main>
+        <el-footer></el-footer>
+    </el-container>
 </template>
