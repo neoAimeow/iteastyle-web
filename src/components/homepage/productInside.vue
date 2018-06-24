f4e1f9088e60586343b30d8d71577cc94a309bce
sharon <403431641@qq.com>
2018-06-24 10:25:41 +0800

    corduct

--- src/components/homepage/productInside.vue
+++ src/components/homepage/productInside.vue
@@ -5,28 +5,49 @@
         <img src="http://pa74otoy6.bkt.clouddn.com/case-light-logo.png">
     </div>
 
+<div>
     <div class="pd-picture">
-        <img class="pd-img" src="http://pa74otoy6.bkt.clouddn.com/product-inside-picture1.png" alt="">
-        <div class="pd-name">
-            <div class="title">
-                <img src="http://pa74otoy6.bkt.clouddn.com/opaque-logo.png" alt="" class="logo">
-                <span>茶饮茶点</span>
-            </div>
-            <span>抹茶费南雪</span>
-        </div>
-        <img class="pd-img" src="http://pa74otoy6.bkt.clouddn.com/product-inside-picture2.png" alt="">
+        <img  v-for="(img,key) in imgs" :key="key"  class="pd-img" :src="img" alt="">        
     </div>
-
+    
+    <div class="pd-name">
+    <div class="title">
+        <img src="http://pa74otoy6.bkt.clouddn.com/opaque-logo.png" alt="" class="logo">
+        <span>茶饮茶点</span>
+    </div>
+    <span>抹茶费南雪</span>
+   </div>
+    
     <div class="pd-introduction">
         <span>抹茶费南雪是一种小糕点<br>就爱这一抹绿啊<br>高油高糖什么的就是好吃<br>抹茶控溺爱的小宠物～</span>
     </div>
 
+    </div>
 </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            imgs:[
+                    "http://pa74otoy6.bkt.clouddn.com/product-inside-picture1.png",
+                    "http://pa74otoy6.bkt.clouddn.com/product-inside-picture2.png",
+                    "http://pa74otoy6.bkt.clouddn.com/product-inside-picture1.png",
+                    "http://pa74otoy6.bkt.clouddn.com/product-inside-picture2.png",
+                    "http://pa74otoy6.bkt.clouddn.com/product-inside-picture1.png"
+
+                  ]
+        }
+    }
+}
+</script>
+
 <style scoped>
     .productinside{
         margin:0;
         padding:0;
+       
 
     }
     .introduction-title{
@@ -45,21 +66,23 @@
     }
     .pd-picture{
         width:1020px;
+        height:220px;
         display:flex;
         flex-direction: row;
+        flex-wrap: wrap;
+        
         margin:0 auto;
         margin-bottom: 20px;
+        background: #9dc135;
+        position: absolute;
+        z-index: -1;
+        
         
 
     }
     .pd-name{
-        width:340px;
-        height:220px;
-        background-color:#9dc135;
-        display:flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
+       
+        
         font-size: 20px;
         color:white;
 
