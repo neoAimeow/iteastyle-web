6d39904e7b5a072da198bc5f14477de01a921e0b
sharon <403431641@qq.com>
2018-07-13 00:26:16 +0800

    tea service

--- src/components/service/teaBreakService.vue
+++ src/components/service/teaBreakService.vue
@@ -1,5 +1,30 @@
 <template>
-    <div style="color:red;">hello world11</div>
+    <div class="service">
+        <div class="reservation">
+            <div class="r-title">
+                <div class="hr"></div>
+                <div class="border">
+                    <div class="name">
+                        <span>茶歇预定</span>
+                        <span>RESERVATION</span>
+                    </div>
+                </div>
+                <div class="hr"></div>
+            </div>
+
+            <span class="res-logo">&#xe9e7;</span>
+            <span>预定方式</span>
+
+            <span class="res-logo">&#xe9e7;</span>
+            <span>茶歇菜单</span>
+            
+        </div>
+       
+    
+    
+    
+    
+    </div>
 </template>
 
 <script>
@@ -9,5 +34,16 @@ export default {
 </script>
 
 <style scoped>
+@font-face {
+  font-family: 'iconfont';  /* project id 721549 */
+  src: url('//at.alicdn.com/t/font_721549_1uk9sekj7xb.eot');
+  src: url('//at.alicdn.com/t/font_721549_1uk9sekj7xb.eot?#iefix') format('embedded-opentype'),
+  url('//at.alicdn.com/t/font_721549_1uk9sekj7xb.woff') format('woff'),
+  url('//at.alicdn.com/t/font_721549_1uk9sekj7xb.ttf') format('truetype'),
+  url('//at.alicdn.com/t/font_721549_1uk9sekj7xb.svg#iconfont') format('svg');
+}
+.res-logo{
+    font-family:iconfont;
 
+}
 </style>
