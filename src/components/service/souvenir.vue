de5ad325fbf3f7ed0a220407ae8b31f6f1895cae
Cross <cross@CrossdeMacBook-Pro.local>
2018-07-14 01:06:53 +0800

    update

--- src/components/service/souvenir.vue
+++ src/components/service/souvenir.vue
@@ -1,5 +1,18 @@
 <template>
-    <div style="color:red;">souvenir</div>
+    <div class="souvenir-container">
+        <div class="souvenir-main">
+            <div class="souvenir-main-title">
+                <span>杭州伴手礼</span>
+                <span>TEA SOUVENIR</span>
+            </div>
+            <div class="souvenir-main-content">
+                <div class="souvenir-main-content-introduce">
+                    <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;西湖龙井、九曲红梅、桂花龙井。都是最具杭城特色的茶品。是赠送外地友人的不二选择，也是外来游客的必选物品。</span>
+                </div>
+                <img class="souvenir-main-content-img" src="http://pa74otoy6.bkt.clouddn.com/case-inside2-picture2.png" alt="">
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -8,6 +21,33 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+.souvenir-container{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+}
+.souvenir-main{
+    width: 900px;
+    background: red;
+}
+.souvenir-main-title{
+    display: flex;
+    flex-direction: column;
+    width: 600px;
+    background: blue;
+}
+.souvenir-main-content{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+}
+.souvenir-main-content-introduce{
+    width: 500px;
+}
+.souvenir-main-content-img{
+    width: 300px;
+    height: 300px;
+}
 </style>
