f5095c8626c3f1cc3a68ae652bfe0aab62858e0e
sharon <403431641@qq.com>
2018-07-14 13:51:09 +0800

    menu-ct

--- src/components/service/teaBreakService.vue
+++ src/components/service/teaBreakService.vue
@@ -47,15 +47,62 @@
                 <span>茶歇菜单</span>
             </div>
 
-            <div class="menu-border">
-                <span>&#xe9ea;</span>
-                <span>点心</span>
+           
 
-                <span>&#xe9ec;</span>
-                <span>水果拼盘</span>
+            <div class="menu-border-out">
+                
+            <div class="menu-border-inside">
+                <div class="menu-head">
+                    <div class="menu-head-name">
+                        <span>A款茶歇/48元人均</span>
+                    </div>
+                    <div class="menu-head-triangle"></div>
+                </div>
 
-                <span>&#xe9eb;</span>
-                <span>饮料</span>
+                <div class="menu-ct">
+                    <div class="menu-left">
+                        <div class="menu-top top1" >
+                            <span>&#xe9ea;</span>
+                            <span>点心</span>
+                        </div>
+                        <ul class="menu-bottom">
+                            <li>特色茶曲奇</li>
+                            <li>抹茶慕斯</li>
+                            <li>水果布丁</li>
+                            <li>抹茶泡芙</li>
+                            <li>双色巧克力蛋糕</li>
+                        </ul>
+                    </div>
+
+                    <div class="menu-mid">
+                        <div class="menu-top">
+                            <span>&#xe9ec;</span>
+                            <span class="top2">水果拼盘</span>
+                        </div>
+                        <ul class="menu-bottom">
+                            <li>西瓜</li>
+                            <li>橙子</li>
+                            <li>圣女果</li>
+                        </ul>
+                    </div>
+                
+                    <div class="menu-right">
+                        <div class="menu-top top3">
+                            <span>&#xe9eb;</span>
+                            <span>饮料</span>
+                        </div>
+                        <ul class="menu-bottom">
+                            <li>抹茶拿铁</li>
+                            <li>桂花龙井茶</li>
+                        </ul>
+                    </div>
+                
+                </div>
+                    
+                <img class="menu-foot" src="http://pa74otoy6.bkt.clouddn.com/menu-foot.png" alt="">
+
+            </div>
+
             </div>
             
         </div>
@@ -68,123 +115,191 @@
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
 @font-face {
-  font-family: 'iconfont';  /* project id 721549 */
-  src: url('//at.alicdn.com/t/font_721549_1uk9sekj7xb.eot');
-  src: url('//at.alicdn.com/t/font_721549_1uk9sekj7xb.eot?#iefix') format('embedded-opentype'),
-  url('//at.alicdn.com/t/font_721549_1uk9sekj7xb.woff') format('woff'),
-  url('//at.alicdn.com/t/font_721549_1uk9sekj7xb.ttf') format('truetype'),
-  url('//at.alicdn.com/t/font_721549_1uk9sekj7xb.svg#iconfont') format('svg');
+  font-family: "iconfont"; /* project id 721549 */
+  src: url("//at.alicdn.com/t/font_721549_1uk9sekj7xb.eot");
+  src: url("//at.alicdn.com/t/font_721549_1uk9sekj7xb.eot?#iefix")
+      format("embedded-opentype"),
+    url("//at.alicdn.com/t/font_721549_1uk9sekj7xb.woff") format("woff"),
+    url("//at.alicdn.com/t/font_721549_1uk9sekj7xb.ttf") format("truetype"),
+    url("//at.alicdn.com/t/font_721549_1uk9sekj7xb.svg#iconfont") format("svg");
 }
 
-.service{
-    margin:0;
-    padding:0;
-}
-    
-.res-logo{
-    font-family:iconfont;
+.service {
+  margin: 0;
+  padding: 0;
 }
 
-.name{
-    text-align: center;
-    background-color: #fff;
-    width:110px;
-    color:#9dc135;   
-    position: absolute;
-    z-index:1;
+.res-logo {
+  font-family: iconfont;
 }
 
-.c-title{
-    display:block; 
-    font-size: 20px; 
-    line-height: 20px;
+.name {
+  text-align: center;
+  background-color: #fff;
+  width: 110px;
+  color: #9dc135;
+  position: absolute;
+  z-index: 1;
 }
 
-.e-title{
-    font-size: 10px;
+.c-title {
+  display: block;
+  font-size: 20px;
+  line-height: 20px;
 }
 
-.border{   
-    border: 2px solid #9dc135;
-    width:160px;
-    height:30px;
-    background-color:#fff;
-    margin-top:20px;   
+.e-title {
+  font-size: 10px;
 }
 
-.mid-name{
-    width:150px;
-    display: block;
-    margin: 0 auto;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.border {
+  border: 2px solid #9dc135;
+  width: 160px;
+  height: 30px;
+  background-color: #fff;
+  margin-top: 20px;
 }
 
-
-.hr{
-    border:0.5px dashed #9dc135;
-    width:400px;
-    position: absolute;
-    z-index: -1;
-    margin-top: 10px;
-
+.mid-name {
+  width: 150px;
+  display: block;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.r-title{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top:30px;
-    
+.hr {
+  border: 0.5px dashed #9dc135;
+  width: 400px;
+  position: absolute;
+  z-index: -1;
+  margin-top: 10px;
 }
 
-.reservation{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+.r-title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
 }
 
-.way,.menu{
-    margin-top:30px;
-    color:#9dc135;
-    font-size: 20px;
+.reservation {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
-.ct1,.ct2,.ct3{
-    
-    width:200px;
-
+.way,
+.menu {
+  margin-top: 30px;
+  color: #9dc135;
+  font-size: 20px;
 }
 
-.way-ct{
-    display: flex;
-    flex-direction: row;
-    margin-top:30px;
+.ct1,
+.ct2,
+.ct3 {
+  width: 200px;
 }
 
-.ct2{
-    margin:0 50px;
+.way-ct {
+  display: flex;
+  flex-direction: row;
+  margin-top: 30px;
+  margin-bottom: 20px;
 }
 
-.ct-title-name{
-    font-size: 18px;
-    margin-left:10px;
-    font-weight:bold;
-    
+.ct2 {
+  margin: 0 50px;
 }
 
-.ct-title{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    margin-bottom:10px;
+.ct-title-name {
+  font-size: 18px;
+  margin-left: 10px;
+  font-weight: bold;
+}
+
+.ct-title {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 10px;
+}
+.menu-ct {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin-bottom: 20px;
+}
+
+.menu-top {
+  color: #9dc135;
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+
+.top1,
+.top3 {
+  letter-spacing: 10px;
+}
+
+.top2 {
+  margin-left: 10px;
+}
+
+.menu-bottom > li {
+  margin-top: 5px;
+}
+
+.menu-border-out {
+  width: 700px;
+  border: 2px solid #9dc135;
+  margin-top: 50px;
+}
+.menu-border-inside {
+  margin: 5px;
+  border: 2px dotted #9dc135;
+}
+
+.menu-head-name {
+  background-color: #9dc135;
+  height: 40px;
+}
+
+.menu-head-name > span {
+  margin: 20px;
+  font-size: 18px;
+  line-height: 40px;
+  color: #fff;
+}
+
+.menu-head-triangle {
+  border-top: solid 20px transparent;
+  border-right: solid 20px transparent;
+  border-bottom: solid 20px transparent;
+  border-left: solid 20px #9dc135;
+}
+
+.menu-head {
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  top: -25px;
+  left: 50px;
+}
+
+.menu-foot {
+  /* position: relative; */
+  float: right;
+  /* right: 0; */
+  /* bottom:50px; */
+  /* left:563px; */
+  background-color: aquamarine;
 }
 </style>
