4ba193832ecd504f363a490f9d3ddd78cf00c114
sharon <403431641@qq.com>
2018-07-15 22:22:40 +0800

    service

--- src/components/service/teaBreakService.vue
+++ src/components/service/teaBreakService.vue
@@ -45,166 +45,41 @@
                                                           
                             <ul class="menu-bottom" v-for="(cake,dessert) in food.desserts" :key="dessert">
                                 <li>{{cake,desserts}}</li>
-                                <!-- <li>抹茶慕斯</li>
-                                <li>水果布丁</li>
-                                <li>抹茶泡芙</li>
-                                <li>双色巧克力蛋糕</li> -->
                             </ul>
                         </div>
 
-                        <!-- <div class="menu">
-                            
-                            <span class="menu-top">&#xe9ec;水果拼盘</span>
-                            
-                            <ul class="menu-bottom">
-                                <li>西瓜</li>
-                                <li>橙子</li>
-                                <li>圣女果</li>
-                            </ul>
-                        </div>
-                
-                        <div class="menu">
-                            <div class="menu-top top3">
-                                <span>&#xe9eb;</span>
-                                <span>饮料</span>
-                            </div>
-                            <ul class="menu-bottom">
-                                <li>抹茶拿铁</li>
-                                <li>桂花龙井茶</li>
-                            </ul>
-                        </div> -->
-                
                     </div>
                     
                 </div>
 
             </div>
 
-        </div>
-        
+        </div>        
 
         <div class="r-title">
             <div class="hr"></div>
             <div class="mid-name">
                 <div class="border"></div>
                 <div class="name">
-                    <span class="c-title">服务范围</span>
-                    <span class="e-title">SERVICE SCOPE</span>
+                    <span class="c-title">{{responseDatas.range.title}}</span>
+                    <span class="e-title">{{responseDatas.range.title_en}}</span>
                 </div>               
             </div>
         </div>
         
         <div class="service-ct">
-            <div class="service-border">
+            <div class="service-border" v-for="(value,key) in responseDatas.range.ranges" :key="key">
                 <div class="service-ct-top">                    
                     <div class="service-ct-top-left">    
-                        <img style="width:30px;height:30px;margin-left:5px;" src="http://pa74otoy6.bkt.clouddn.com/opaque-logo.png" alt="logo">
-                        <div class="service-e-name">Company Afternon Tea</div>
+                        <img class="service-ct-top-no" :src=value.titleImageUrl alt="logo">
+                        <div class="service-e-name">{{value.title_en}}</div>
                     </div>
                     <div class="service-c-name">
-                        <span>公司</span>
-                        <span class="s-c-name-bottom">下午茶</span>
+                        <span>{{value.mainTitle}}</span>
+                        <span class="s-c-name-bottom">{{value.subTitle}}</span>
                     </div>
                 </div>
-                <img style="width:180px;height:120px;" src="http://pa74otoy6.bkt.clouddn.com/case-inside2-picture1.png" alt="service picture">
-            </div>
-
-            <div class="service-border">
-                <div class="service-ct-top">                    
-                    <div class="service-ct-top-left">    
-                        <img style="width:30px;height:30px;margin-left:5px;" src="http://pa74otoy6.bkt.clouddn.com/opaque-logo.png" alt="logo">
-                        <div class="service-e-name">Staff Birthday Party</div>
-                    </div>
-                    <div class="service-c-name">
-                        <span>员工</span>
-                        <span class="s-c-name-bottom">生日会</span>
-                    </div>
-                </div>
-                <img style="width:180px;height:120px;" src="http://pa74otoy6.bkt.clouddn.com/case-inside2-picture1.png" alt="service picture">
-            </div>
-
-            <div class="service-border">
-                <div class="service-ct-top">                    
-                    <div class="service-ct-top-left">    
-                        <img style="width:30px;height:30px;margin-left:5px;" src="http://pa74otoy6.bkt.clouddn.com/opaque-logo.png" alt="logo">
-                        <div class="service-e-name">Annual Meeting</div>
-                    </div>
-                    <div class="service-c-name">
-                        <span>公司</span>
-                        <span class="s-c-name-bottom">年会</span>
-                    </div>
-                </div>
-                <img style="width:180px;height:120px;" src="http://pa74otoy6.bkt.clouddn.com/case-inside2-picture1.png" alt="service picture">
-            </div>
-
-            <div class="service-border">
-                <div class="service-ct-top">                    
-                    <div class="service-ct-top-left">    
-                        <img style="width:30px;height:30px;margin-left:5px;" src="http://pa74otoy6.bkt.clouddn.com/opaque-logo.png" alt="logo">
-                        <div class="service-e-name">Festival Celebration</div>
-                    </div>
-                    <div class="service-c-name">
-                        <span>节日</span>
-                        <span class="s-c-name-bottom">庆典</span>
-                    </div>
-                </div>
-                <img style="width:180px;height:120px;" src="http://pa74otoy6.bkt.clouddn.com/case-inside2-picture1.png" alt="service picture">
-            </div>
-
-            <div class="service-border">
-                <div class="service-ct-top">                    
-                    <div class="service-ct-top-left">    
-                        <img style="width:30px;height:30px;margin-left:5px;" src="http://pa74otoy6.bkt.clouddn.com/opaque-logo.png" alt="logo">
-                        <div class="service-e-name">High-end Reception</div>
-                    </div>
-                    <div class="service-c-name">
-                        <span>高端</span>
-                        <span class="s-c-name-bottom">酒会</span>
-                    </div>
-                </div>
-                <img style="width:180px;height:120px;" src="http://pa74otoy6.bkt.clouddn.com/case-inside2-picture1.png" alt="service picture">
-            </div>
-
-            <div class="service-border">
-                <div class="service-ct-top">                    
-                    <div class="service-ct-top-left">    
-                        <img style="width:30px;height:30px;margin-left:5px;" src="http://pa74otoy6.bkt.clouddn.com/opaque-logo.png" alt="logo">
-                        <div class="service-e-name">Business Reception</div>
-                    </div>
-                    <div class="service-c-name">
-                        <span>商务</span>
-                        <span class="s-c-name-bottom">接待会</span>
-                    </div>
-                </div>
-                <img style="width:180px;height:120px;" src="http://pa74otoy6.bkt.clouddn.com/case-inside2-picture1.png" alt="service picture">
-            </div>
-
-            <div class="service-border">
-                <div class="service-ct-top">                    
-                    <div class="service-ct-top-left">    
-                        <img style="width:30px;height:30px;margin-left:5px;" src="http://pa74otoy6.bkt.clouddn.com/opaque-logo.png" alt="logo">
-                        <div class="service-e-name">Product Launch</div>
-                    </div>
-                    <div class="service-c-name">
-                        <span>产品</span>
-                        <span class="s-c-name-bottom">发布会</span>
-                    </div>
-                </div>
-                <img style="width:180px;height:120px;" src="http://pa74otoy6.bkt.clouddn.com/case-inside2-picture1.png" alt="service picture">
-            </div>
-
-            <div class="service-border">
-                <div class="service-ct-top">                    
-                    <div class="service-ct-top-left">    
-                        <img style="width:30px;height:30px;margin-left:5px;" src="http://pa74otoy6.bkt.clouddn.com/opaque-logo.png" alt="logo">
-                        <div class="service-e-name">Opening Ceremony</div>
-                    </div>
-                    <div class="service-c-name">
-                        <span>开业</span>
-                        <span class="s-c-name-bottom">庆典</span>
-                    </div>
-                </div>
-                <img style="width:180px;height:120px;" src="http://pa74otoy6.bkt.clouddn.com/case-inside2-picture1.png" alt="service picture">
+                <img style="width:180px;height:120px;" :src=value.imageUrl alt="service picture">
             </div>
 
         </div>
@@ -214,97 +89,22 @@
             <div class="mid-name">
                 <div class="border"></div>
                 <div class="name">
-                    <span class="c-title">订购流程</span>
-                    <span class="e-title">ORDER PROCESS</span>
+                    <span class="c-title">{{responseDatas.process.title}}</span>
+                    <span class="e-title">{{responseDatas.process.title_en}}</span>
                 </div>               
             </div>
         </div>
 
         <div class="order">
-            <div class="order-ct">
+            <div class="order-ct" v-for="(value,key) in responseDatas.process.processes" :key="key">
                 <div>
                     <div class="order-top">
-                        <span>1、售前咨询</span>
+                        <span>{{key+1}}.{{value.title}}</span>
                     </div>
-                    <div class="order-bottom"><span>茶歇服务完成后，请于当日内支付尾款</span></div>                
+                    <div class="order-bottom"><span>{{value.content}}</span></div>                
                 </div>
                 <span class="order-arrow">&#xe631;</span>
             </div>
-
-            <div class="order-ct">
-                <div>
-                    <div class="order-top">
-                        <span>2、确认订单</span>
-                    </div>
-                    <div class="order-bottom"><span>确认选择的产品和服务清单</span></div>                
-                </div>
-                <span class="order-arrow">&#xe631;</span>
-            </div>
-
-            <div class="order-ct">
-                <div>
-                    <div class="order-top">
-                        <span>3、线上预定</span>
-                    </div>
-                    <div class="order-bottom"><span>线上预定</span></div>                
-                </div>
-                <span class="order-arrow">&#xe631;</span>
-            </div>
-
-            <div class="order-ct">
-                <div>
-                    <div class="order-top">
-                        <span>4、接单合作</span>
-                    </div>
-                    <div class="order-bottom"><span>根据合同的产品清单制作新鲜的茶歇产品</span></div>                
-                </div>
-                <span class="order-arrow">&#xe631;</span>
-            </div>
-
-            <div class="order-ct">
-                <div>
-                    <div class="order-top">
-                        <span>5、订单配送</span>
-                    </div>
-                    <div class="order-bottom"><span>按合同上约定的送达时间准时送达产品</span></div>                
-                </div>
-                <span class="order-arrow">&#xe631;</span>
-            </div>
-
-            <div class="order-ct">
-                <div>
-                    <div class="order-top">
-                        <span>6、摆台服务</span>
-                    </div>
-                    <div class="order-bottom"><span>现场服务人员布置茶歇摆台与装饰</span></div>                
-                </div>
-                <span class="order-arrow">&#xe631;</span>
-            </div>
-
-            <div class="order-ct">
-                <div>
-                    <div class="order-top">
-                        <span>7、售后服务</span>
-                    </div>
-                    <div class="order-bottom"><span>对产品及服务有不满意的地方请及时联系客服小tea</span></div>                
-                </div>
-                <span class="order-arrow">&#xe631;</span>
-            </div>
-
-            <div class="order-ct">
-                <div>
-                    <div class="order-top">
-                        <span>8、支付尾款</span>
-                    </div>
-                    <div class="order-bottom"><span>电话或微信联系杭式下午茶小tea，了解产品及服务</span></div>                
-                </div>
-                <span class="order-arrow">&#xe631;</span>
-            </div>
-
-
-            
-
-
         </div>
     
         
@@ -314,62 +114,19 @@
                 <div class="mid-name">
                     <div class="border"></div>
                     <div class="name">
-                        <span class="c-title">订购须知</span>
-                        <span class="e-title">ORDERING INSTRUCTIONS</span>
+                        <span class="c-title">{{responseDatas.note.title}}</span>
+                        <span class="e-title">{{responseDatas.note.title_en}}</span>
                     </div>               
                 </div>
             </div>
 
             <div class="in-ct">
-                <div class="in-ct1">
+                <div class="in-ct1" v-for="(value,key) in responseDatas.note.notes" :key="key">
                     <div class="in-no">
-                        <span class="in-no1">1</span>
+                        <span class="in-no1">{{key+1}}</span>
                     </div>
-                    <span>杭式下午茶目前支持杭州地区订购，全国其他地区暂未开通；</span>
+                    <span v-html="value"></span>
                 </div>
-
-                <div class="in-ct1">
-                    <div class="in-no">
-                        <span class="in-no1">2</span>
-                    </div>
-                    <span>订单金额1500元起订，所有订单请提前48小时预约并支付预定金；</span>
-                </div>
-
-                <div class="in-ct1">
-                    <div class="in-no">
-                        <span class="in-no1">3</span>
-                    </div>
-                    <span>高端个性化定制请提前7天预约；</span>
-                </div>
-
-                <div class="in-ct1">
-                    <div class="in-no">
-                        <span class="in-no1">4</span>
-                    </div>
-                    <span>杭州市区内免费送货，其他地区（余杭、萧山等）交通费另计；</span>
-                </div>
-
-                <div class="in-ct1">
-                    <div class="in-no">
-                        <span class="in-no1">5</span>
-                    </div>
-                    <span>杭式下午茶提供茶歇现场服务人员，并提供现场摆盘服务、装饰布置等服务；</span>
-                </div>
-
-                <div class="in-ct1">
-                    <div class="in-no">
-                        <span class="in-no1">6</span>
-                    </div>
-                    <span>活动结束后，现场器具如有丢失损坏，100元金额以内属正常损耗，无须赔偿，100元金额以上需按器具价格进行赔偿；</span>
-                </div>
-
-                <div class="in-ct1">
-                    <div class="in-no">
-                        <span class="in-no1">7</span>
-                    </div>
-                    <span>如在订购后解除订购，我们将按照实际制作量按比例扣除预定订金。</span>
-                </div>
-
             </div>
         </div>
     
@@ -379,8 +136,8 @@
                 <div class="mid-name">
                     <div class="border"></div>
                     <div class="name">
-                        <span class="c-title">订购方式</span>
-                        <span class="e-title">ORDERING WAYS</span>
+                        <span class="c-title">{{responseDatas.orderMode.title}}</span>
+                        <span class="e-title">{{responseDatas.orderMode.title_en}}</span>
                     </div>               
                 </div>
             </div>
@@ -388,11 +145,11 @@
             <div class="ways-ct">
                 <div class="ways-top">
                     <div class="ways-ver"></div>
-                    <span>公众号预定</span>
+                    <span>{{responseDatas.orderMode.qrTitle}}</span>
                     <div class="ways-ver"></div>
                 </div>
                 <div class="ways-bottom">
-                    <img class="ways-bottom-left" src="http://pa74otoy6.bkt.clouddn.com/opaque-logo.png" alt="QR Code">
+                    <img class="ways-bottom-left" :src=responseDatas.orderMode.qrImageUrl alt="QR Code">
                     <div class="ways-bottom-mid"></div>
                     <div class="ways-bottom-right">
                         <span>长按二维码关注</span>
@@ -572,7 +329,7 @@ export default {
 }
 
 .menu-border-out {
-  width: 900px;
+  width: 700px;
   border: 2px solid #9dc135;
   margin-top: 50px;
 }
@@ -611,11 +368,15 @@ export default {
 .service-ct-top {
   display: flex;
   flex-direction: row;
-
   /* background-color: yellow; */
   margin-bottom: 5px;
 }
 
+.service-ct-top-no{
+  width:30px;
+  height:30px;
+  margin-left:5px;
+}
 .service-c-name {
   display: flex;
   flex-direction: column;
@@ -645,6 +406,7 @@ export default {
   justify-content: center;
   align-items: center;
   box-shadow: 0px 0px 10px 0px #e0e0e0;
+  margin-bottom:30px;
 }
 
 .s-c-name-bottom {
@@ -662,7 +424,6 @@ export default {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
-  height: 450px;
   margin: 0 auto;
   margin-top: 60px;
 }
@@ -674,6 +435,7 @@ export default {
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  margin-bottom:30px;
 }
 
 .order-top {
@@ -704,7 +466,6 @@ export default {
 
 .order {
   width: 900px;
-  height: 400px;
   margin: 0 auto;
   display: flex;
   flex-wrap: wrap;
